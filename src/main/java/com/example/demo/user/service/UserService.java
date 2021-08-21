179c795d00cebda6c20fffcbbacfef4df9108384
sunhwa-kim <ksh0810@gmail.com>
2021-08-22 04:24:23 +0900

    UserService and UserResponse

--- src/main/java/com/example/demo/user/service/UserService.java
+++ src/main/java/com/example/demo/user/service/UserService.java
@@ -1,7 +1,23 @@
 package com.example.demo.user.service;
 
+import com.example.demo.user.model.dto.UserDto;
+import com.example.demo.user.model.dto.UserResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public UserResponse newUser(UserDto dto) {
+        var user = new User(dto.getName(), dto.getEmail(), dto.getPassword());
+        User newUser = userRepository.save(user);
+        System.out.println(newUser.getRegisteredAt().toString());
+        return UserResponse.builder()
+                .name(newUser.getName())
+                .email(newUser.getEmail())
+                .build();
+    }
 }
